0a45a43e111fec5c64749d52cd22874e4fb74e9d
mouss2388 <mouss2388@gmail.com>
2021-01-16 12:59:19 +0100

    Feat getCurrentDate for news

--- js/app.js
+++ js/app.js
@@ -1,22 +1,34 @@
-const input = document.querySelector('input');
-const btnClear = document.getElementsByClassName('clear');
+const input = document.querySelector("input");
+const btnClear = document.getElementsByClassName("clear");
+const news = document.getElementById("dateNews");
 
 input.oninput = handleInput;
-
+getCurrentDate();
 
 function handleInput(event) {
-
-    event.preventDefault();
-    if (input.value === "") {
-        btnClear[0].classList.add('hidden');
-    } else {
-        btnClear[0].classList.remove('hidden');
-        btnClear.item(0).addEventListener('click', () => {
-            input.value = "";
-            input.focus();
-        })
-    }
-    
+  event.preventDefault();
+  if (input.value === "") {
+    btnClear[0].classList.add("hidden");
+  } else {
+    btnClear[0].classList.remove("hidden");
+    btnClear.item(0).addEventListener("click", () => {
+      input.value = "";
+      input.focus();
+    });
+  }
 }
 
+function getCurrentDate() {
+  var today = new Date();
+  var day = String(today.getDate()).padStart(2, "0");
 
+  let monthIdx = today.getMonth();
+  let monthsArray = new Array("Janvier", "Février");
+  let month = monthsArray[monthIdx];
+
+  let year = today.getFullYear();
+
+  today = day + " " + month + " " + year;
+  news.textContent = today;
+  console.log(today);
+}
